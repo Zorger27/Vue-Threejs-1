d0adc050aeda66887b1fdd18cea10293a19c8681
Anatolii Zorin <Zorger27@GMail.com>
2023-12-19 03:59:10 +0200

    Small changes

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,7 +11,7 @@ const routes: Array<RouteRecordRaw> = [
     name: 'project1',
     component: Project1,
     meta: {
-      title: 'Three.js projects - Project № 1',
+      title: 'Three.js - Project № 1',
       metaTags: [
         {
           property: 'og:title',
@@ -41,7 +41,7 @@ const routes: Array<RouteRecordRaw> = [
     name: 'project2',
     component: Project2,
     meta: {
-      title: 'Three.js projects - Project № 2',
+      title: 'Three.js - Project № 2',
       metaTags: [
         {
           property: 'og:title',
@@ -71,7 +71,7 @@ const routes: Array<RouteRecordRaw> = [
     name: 'project3',
     component: Project3,
     meta: {
-      title: 'Three.js projects - Project № 3',
+      title: 'Three.js - Project № 3',
       metaTags: [
         {
           property: 'og:title',
