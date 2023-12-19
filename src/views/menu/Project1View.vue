027f481cd044a95a3e21fa19662f231eb8de6573
Anatolii Zorin <Zorger27@GMail.com>
2023-12-19 05:37:55 +0200

    Small changes

--- src/views/menu/Project1View.vue
+++ src/views/menu/Project1View.vue
@@ -1,19 +1,79 @@
-<script lang="ts">
-import {Options, Vue} from 'vue-class-component';
+<script>
+import { ref, onMounted, onUnmounted } from 'vue';
+import * as THREE from 'three';
 
-@Options({
-  methods: {},
-  components: {},
-})
-export default class Project1 extends Vue {
+export default {
+  name: 'ThreeScene',
+  setup() {
+    const canvasContainer = ref(null);
+    let scene, camera, renderer, cube;
+
+    const init = () => {
+      // Создаем сцену
+      scene = new THREE.Scene();
+
+      // Создаем камеру
+      camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+      camera.position.z = 5;
+
+      // Создаем рендерер
+      renderer = new THREE.WebGLRenderer();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+
+      // Создаем геометрию и материал для куба
+      const geometry = new THREE.BoxGeometry();
+      const materials = [
+        new THREE.MeshBasicMaterial({ color: 0xff0000 }),
+        new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
+        new THREE.MeshBasicMaterial({ color: 0x0000ff }),
+        new THREE.MeshBasicMaterial({ color: 0xffff00 }),
+        new THREE.MeshBasicMaterial({ color: 0xff00ff }),
+        new THREE.MeshBasicMaterial({ color: 0x00ffff }),
+      ];
+      cube = new THREE.Mesh(geometry, materials);
+
+      // Добавляем куб на сцену
+      scene.add(cube);
+
+      // Добавляем рендерер в контейнер
+      canvasContainer.value.appendChild(renderer.domElement);
+
+      // Обновляем сцену
+      const animate = () => {
+        requestAnimationFrame(animate);
+
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+
+        renderer.render(scene, camera);
+      };
+
+      animate();
+    };
+
+    onMounted(() => {
+      init();
+    });
+
+    onUnmounted(() => {
+      // Выполняем необходимые действия при удалении компонента
+      // Например, очищаем ресурсы Three.js
+      renderer.dispose();
+    });
+
+    return {
+      canvasContainer,
+    };
+  },
 };
 </script>
 
 <template>
-  <div class="container">
-    <h1>{{ $t('project1.name') }}</h1>
-    <line></line>
-  </div>
+<!--  <div class="container">-->
+<!--    <h1>{{ $t('project1.name') }}</h1>-->
+<!--    <line></line>-->
+    <div ref="canvasContainer"></div>
+<!--  </div>-->
 </template>
 
 <style lang="scss" scoped>
