e92b9e40ad89dd9fd386ba31d2e0c3ec8aa74943
Anatolii Zorin <Zorger27@GMail.com>
2024-12-18 04:42:20 +0200

    Small changes

--- src/store/modules/service/infoStore.ts
+++ src/store/modules/service/infoStore.ts
@@ -16,13 +16,13 @@ const state = {
       {
         id: 3,
         title: "Vue.js",
-        version: "3.4.21",
+        version: "3.5.13",
         url: "https://v3.ru.vuejs.org"
       },
       {
         id: 4,
         title: "Vue Router",
-        version: "4.3.0",
+        version: "4.5.0",
         url: "https://router.vuejs.org"
       },
       {
@@ -39,33 +39,69 @@ const state = {
       },
       {
         id: 7,
+        title: "Webpack",
+        version: "5.97.1",
+        url: "https://webpack.js.org"
+      },
+      {
+        id: 8,
         title: "HTML5",
         version: "",
         url: "https://developer.mozilla.org/ru/docs/Learn/HTML/Introduction_to_HTML"
       },
       {
-        id: 8,
+        id: 9,
         title: "CSS3",
         version: "",
         url: "https://developer.mozilla.org/ru/docs/Learn/CSS/First_steps"
       },
       {
-        id: 9,
+        id: 10,
         title: "FlexBox CSS",
         version: "",
         url: "https://developer.mozilla.org/ru/docs/Learn/CSS/CSS_layout/Flexbox"
       },
       {
-        id: 10,
+        id: 11,
         title: "JavaScript",
         version: "",
         url: "https://developer.mozilla.org/ru/docs/Learn/JavaScript"
       },
       {
-        id: 11,
+        id: 12,
         title: "TypeScript",
         version: "5.7.2",
         url: "https://www.typescriptlang.org/"
+      },
+      {
+        id: 13,
+        title: "Markdown",
+        version: "",
+        url: "https://www.markdownguide.org"
+      },
+      {
+        id: 14,
+        title: "Open Graph protocol",
+        version: "",
+        url: "https://ogp.me"
+      },
+      {
+        id: 15,
+        title: "Favicon",
+        version: "",
+        url: "https://developer.mozilla.org/en-US/docs/Glossary/Favicon"
+      },
+      {
+        id: 16,
+        title: "Sitemap",
+        version: "",
+        url: "https://www.sitemaps.org"
+      },
+      {
+        id: 17,
+        title: "Robots.txt",
+        version: "",
+        url: "https://developers.google.com/search/docs/crawling-indexing/robots/intro"
       },
     ]
 };
